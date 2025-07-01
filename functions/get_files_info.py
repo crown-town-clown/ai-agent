1052e1a903a0ee37ee1b894e4ca11789b4d9d554
crown-town-clown <63020400+crown-town-clown@users.noreply.github.com>
2025-07-01 02:59:29 -0700

    updated get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,32 +4,25 @@ import os
 #define get files function that will accept a directory path, and return a string that represents
 #contents of that directory
 def get_files_info(working_directory, directory=None):
-	os.chdir("/home/yuh/workspace/github.com/crown-town-clown/ai-agent")
-	for root, dirs, files in os.walk(os.getcwd()):
-		if working_directory in root and directory == ".":
-			#build and return a string representing the contents of the directory
-			dir_contents = f"Result for current directory:\n"
-			#add each file's name, size in bytes, and whether it's a directory or not to the string
-			for element in os.listdir(root):
-				element_path = os.path.join(root, element)
-				element_contents = f"- {element}: file_size={os.path.getsize(element_path)} bytes, is_dir={os.path.isdir(element_path)}\n"
-				dir_contents += element_contents 
-			return dir_contents
-		if working_directory in root and directory in dirs:
-			#build and return a string representing the contents of the directory
-			dir_contents = f"Result for '{directory}' directory:\n"
-			dir_path = os.path.join(root, directory)
-			#add each file's name, size in bytes, and whether it's a directory or not to the string
-			for element in os.listdir(dir_path):
-				element_path = os.path.join(dir_path, element)
-				element_contents = f"- {element}: file_size={os.path.getsize(element_path)} bytes, is_dir={os.path.isdir(element_path)}\n"
-				dir_contents += element_contents
-			return dir_contents
-		elif working_directory in root and directory not in dirs:
-			if directory in files:
-				return f"Error: '{directory}' is not a directory"
-			else:
-				return f"Error: Cannot list '{directory}' as it is outside the permitted working directory"
-
-
-#print(get_files_info("calculator", "."))
+	abs_working_dir = os.path.abspath(working_directory)
+	target_dir = abs_working_dir
+	#test if directory is outside working_directory
+	if directory:
+		target_dir = os.path.abspath(os.path.join(working_directory, directory))
+	if not target_dir.startswith(abs_working_dir):
+		return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+	if not os.path.isdir(target_dir):
+		return f'Error: "{directory}" is not a directory'
+	#build and return a string representing the contents of the directory
+	dir_contents = []
+	#add each file's name, size in bytes, and whether it's a directory or not to the string
+	try:
+		for element in os.listdir(target_dir):
+			element_path = os.path.join(target_dir, element)
+			size = os.path.getsize(element_path)
+			is_dir = os.path.isdir(element_path)
+			file_content = f"- {element}: file_size={size} bytes, is_dir={is_dir}"
+			dir_contents.append(file_content)
+	except Exception as e:
+		return f'Error list files: {e}'
+	return "\n".join(dir_contents)

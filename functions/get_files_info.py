5af4b7bc8b5ccbf2ac162a6f3531a0600fee3454
crown-town-clown <63020400+crown-town-clown@users.noreply.github.com>
2025-07-01 00:54:17 -0700

    updated get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,13 +7,24 @@ def get_files_info(working_directory, directory=None):
 	os.chdir("/home/yuh/workspace/github.com/crown-town-clown/ai-agent")
 	for root, dirs, files in os.walk(os.getcwd()):
 		if working_directory in root and directory == ".":
-			return "current"
-		if working_directory in root and directory == "..":
-			return "parent"
+			#build and return a string representing the contents of the directory
+			dir_contents = f"Result for current directory:\n"
+			#add each file's name, size in bytes, and whether it's a directory or not to the string
+			for element in os.listdir(root):
+				element_path = os.path.join(root, element)
+				element_contents = f"- {element}: file_size={os.path.getsize(element_path)} bytes, is_dir={os.path.isdir(element_path)}\n"
+				dir_contents += element_contents 
+			return dir_contents
 		if working_directory in root and directory in dirs:
 			#build and return a string representing the contents of the directory
-
-			return "booyah"
+			dir_contents = f"Result for '{directory}' directory:\n"
+			dir_path = os.path.join(root, directory)
+			#add each file's name, size in bytes, and whether it's a directory or not to the string
+			for element in os.listdir(dir_path):
+				element_path = os.path.join(dir_path, element)
+				element_contents = f"- {element}: file_size={os.path.getsize(element_path)} bytes, is_dir={os.path.isdir(element_path)}\n"
+				dir_contents += element_contents
+			return dir_contents
 		elif working_directory in root and directory not in dirs:
 			if directory in files:
 				return f"Error: '{directory}' is not a directory"
@@ -25,7 +36,7 @@ def get_files_info(working_directory, directory=None):
 
 #print(os.getcwd())
 
-print(get_files_info("calculator", "../"))
+print(get_files_info("calculator", "."))
 
 
 
